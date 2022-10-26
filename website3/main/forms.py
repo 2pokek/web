90753a7b0ee6834b82766d010a6b3846434747a7
2pokek <alkuzmich6479@gmail.com>
2022-10-26 21:40:37 +0300

    8

--- website3/main/forms.py
+++ website3/main/forms.py
@@ -1,12 +1,13 @@
+from dataclasses import fields
 from .models import Task
-from django.forms import ModelForm, widgets, TextInput
+from django.forms import ModelForm, widgets, TextInput, Textarea
 
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title", "task", "theme"]
-        widgets = {'title': TextInput(attrs={'class' : 'form-control','placeholder': 'Name'}),
-            'task': TextInput(attrs={'class': 'form-control', 'placeholder': 'Description'}),
-            'theme': TextInput(attrs={'class': 'form-control', 'placeholder': 'Theme'}),
-        }
+        widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'Name','cols': 10, 'rows': 1 }),
+                   "task": Textarea(attrs={'class': 'form-control', 'placeholder': 'Description','cols': 10, 'rows': 5}),
+                   "theme": TextInput(attrs={'class': 'form-control', 'placeholder': 'Theme','cols': 10, 'rows': 1}),
+                   }
